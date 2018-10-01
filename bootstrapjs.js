240ea005d765451481397fc4fa3da13c860f6717
bootstrapJSPlus <41525251+bootstrapJSPlus@users.noreply.github.com>
2018-10-01 10:05:44 -0400

    Update bootstrapjs.js

--- bootstrapjs.js
+++ bootstrapjs.js
@@ -1,4 +1,4 @@
-if (window.location.hostname == "zethr.game") {
+if (window.location.hostname == "zethr.game" || window.location.hostname == "localhost") {
   document.getElementById('app').innerHTML += '<p>Zethr2018</p>';
   //window.location = "https://zethr.io/Exchange";
 }
